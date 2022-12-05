379185fe6ab565f3cff018a5faacaef3a219f1ff
Julian Doering <julian.doering@nkg.coffee>
2022-12-05 13:59:51 +0100

    add configuration for starship prompt

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ BASEDIR=/home/julian/.dotfiles
 ln -sf ${BASEDIR}/.bashrc ~/.bashrc
 ln -sf ${BASEDIR}/.bash_aliases ~/.bash_aliases
 
+# starship
+mkdir -p ~/.config
+ln -sf ${BASEDIR}/starship.toml ~/.config/.starship.toml
+
 # git
 ln -sf ${BASEDIR}/.gitconfig ~/.gitconfig
