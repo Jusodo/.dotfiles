1016a44d90a66bde96a5a7996cff89663ee8abca
Julian Doering <julian.doering@nkg.coffee>
2023-01-13 12:57:07 +0100

    (nvim) make sure javascript is installed for treesitter

--- nvim/lua/julian/treesitter.lua
+++ nvim/lua/julian/treesitter.lua
@@ -11,7 +11,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+  ignore_install = { },
 
   highlight = {
     -- `false` will disable the whole extension
