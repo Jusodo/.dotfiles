b48363483b00ceeb35d36dc8c6f283d51436c448
Julian Doering <julian.doering@nkg.coffee>
2023-01-13 12:57:57 +0100

    (nvim) install packer if not available

--- nvim/lua/julian/packer.lua
+++ nvim/lua/julian/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -68,13 +68,13 @@ return require('packer').startup(function(use)
 
     -- bufferline
     use {
-        'akinsho/bufferline.nvim', 
+        'akinsho/bufferline.nvim',
         tag = "v3.*", 
         requires = 'nvim-tree/nvim-web-devicons'
     }
 
     -- lualine
-        use {
+    use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
