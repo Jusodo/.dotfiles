cff0355850ce1a8bc568d6161afdbb31b815a820
Julian Doering <julian.doering@nkg.coffee>
2023-01-16 11:05:59 +0100

    (nvim) add onedark theme for lualine

--- nvim/lua/julian/lualine.lua
+++ nvim/lua/julian/lualine.lua
@@ -1,1 +1,21 @@
-require('lualine').setup()
+local lualine_loaded, lualine = pcall(require, "lualine")
+if not lualine_loaded then
+    print("lualine not loaded")
+    return
+end
+
+lualine.setup{
+    options = {
+        icons_enabled = true,
+        theme = "onedark",
+        component_seperators = "|",
+        section_seperators = "",
+    },
+    sections = {
+        lualine_a = {
+            {
+                "buffers",
+            }
+        }
+    }
+}
